23a9ab003a81c1a598652c090ec649d09cc3ae3b
kamrulhasan <kamrulhasan3288@gmail.com>
2020-05-07 13:21:44 +0600

    add ChildViewHolder

---
+++ expandablerecyclerviewlist/src/main/java/com/iamkamrul/expandablerecyclerviewlist/viewholder/ChildViewHolder.kt
@@ -0,0 +1,6 @@
+package com.iamkamrul.expandablerecyclerviewlist.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+open class ChildViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
